53020108b5cc1c21543a0ecbb2fde75672f9cd5d
Project Therion AI <140287105+THERIONAI@users.noreply.github.com>
2024-12-13 01:32:32 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.fraud_detection import detect_fraud
+from src.governance import governance_mechanism
+
+if __name__ == "__main__":
+    print("Welcome to Project Therion")
+    # Example usage
+    detect_fraud()
+    governance_mechanism()
